dbb41ad27492f323c5d3c30e0128c5fd83849cbe
Ricardo Tosi Bela <69743502+Rjasao@users.noreply.github.com>
2022-09-26 20:47:13 -0300

    Remove do MySQL

--- aulajpamaven/src/main/java/aplicacao/Programa.java
+++ aulajpamaven/src/main/java/aplicacao/Programa.java
@@ -14,10 +14,12 @@ public class Programa {
 		EntityManager em = emf.createEntityManager();
 		
 		Pessoa p = em.find(Pessoa.class, 2);
-		
-		System.out.println(p);
+		em.getTransaction().begin();
+		em.remove(p); // Remove do MySQL
+		em.getTransaction().commit();
 		
 		System.out.println("Pronto");
+		
 		em.clear();
 		emf.close();
 	}
