80700955ae10a382ef216e8b8787c60f8140f056
Ricardo Tosi Bela <69743502+Rjasao@users.noreply.github.com>
2022-09-28 01:31:27 -0300

    Nivelamento-sobre-JPA
    Acessa dados - apostila 24

--- aulajpamaven/src/main/java/aplicacao/Programa.java
+++ aulajpamaven/src/main/java/aplicacao/Programa.java
@@ -9,23 +9,21 @@ import dominio.Pessoa;
 public class Programa {
 
 	public static void main(String[] args) {
-
-		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com"); 
-		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com"); 
-		Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
 	
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
 		
 		em.getTransaction().begin();//inicial p savar no Bco de dados
 		
-		em.persist(p1);
-		em.persist(p2);
-		em.persist(p3);
+		Pessoa p = em.find(Pessoa.class, 5);//acessa dados
+		
+		System.out.println(p);
 		
 		em.getTransaction().commit();//concluir salvamento
 		
 		System.out.println("Pronto");
+		emf.close();
+		em.close();
 	}
 
 }
